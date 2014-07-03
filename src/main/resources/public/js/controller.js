72b7195bff749acbe0f253c0103930e739782b06
Camille Wintz <camille.wintz@web-education.net>
2014-07-03 18:16:39 +0200

    basic interface

--- src/main/resources/public/js/controller.js
+++ src/main/resources/public/js/controller.js
@@ -1,3 +1,54 @@
-function ForumController($scope, template){
-	$scope.template = template;
+function ForumController($scope, template, model, date){
+	template.open('main', 'categories');
+
+	$scope.categories = model.categories;
+	$scope.date = date;
+
+	$scope.display = {};
+
+	$scope.message = new Message();
+
+	$scope.selectAll = function(){
+		if($scope.display.selectSubjects){
+			$scope.category.selectAll();
+		}
+		else{
+			$scope.category.deselectAll();
+		}
+	};
+
+	$scope.openCategory = function(category){
+		$scope.category = category;
+		$scope.subjects = category.subjects;
+		category.open();
+		template.open('main', 'subjects');
+	};
+
+	$scope.openSubject = function(subject){
+		$scope.subject = subject;
+		subject.open();
+		template.open('main', 'read-subject');
+		$scope.messages =  subject.messages;
+	};
+
+	$scope.newSubject = function(){
+		$scope.subject = new Subject();
+		template.open('main', 'new-subject');
+	};
+
+	$scope.addSubject = function(){
+		$scope.category.addSubject($scope.subject);
+		$scope.subject.addMessage($scope.message);
+		template.open('main', 'read-subject');
+	};
+
+	$scope.closeSubject = function(){
+		$scope.subject = undefined;
+		template.open('main', 'subjects');
+	};
+
+	$scope.addMessage = function(){
+		$scope.subject.addMessage($scope.message);
+		$scope.message = new Message();
+	}
 }
--- src/main/resources/public/js/model.js
+++ src/main/resources/public/js/model.js
@@ -1,15 +1,78 @@
-function Category(){
-
-}
-
-function Thread(){
-
-}
-
 function Message(){
 
 }
 
-model.build = function(){
-	this.makeModels([Category, Thread, Message]);
+Message.prototype.createMessage = function(){
+	notify.info('Message envoyé');
+};
+
+Message.prototype.editMessage = function(){
+
+};
+
+Message.prototype.save = function(){
+	if(!this.id){
+		this.createMessage();
+	}
+	else{
+		this.editMessage();
+	}
+};
+
+function Subject(){
+	var subject = this;
+
+	this.collection(Message, {
+		sync: function(){
+			http().get('/forum/public/json/messages-' + subject.id + '.json').done(function(messages){
+				this.load(messages);
+			}.bind(this));
+		}
+	});
 }
+
+Subject.prototype.open = function(){
+	this.messages.sync();
+};
+
+Subject.prototype.addMessage = function(message){
+	this.messages.push(message);
+	message.save();
+};
+
+Subject.prototype.save = function(){
+
+};
+
+function Category(){
+	var category = this;
+
+	this.collection(Subject, {
+		sync: function(){
+			http().get('/forum/public/json/subjects-' + category.id + '.json').done(function(subjects){
+				this.load(subjects);
+			}.bind(this))
+		}
+	});
+}
+
+Category.prototype.open = function(){
+	this.subjects.sync();
+};
+
+Category.prototype.addSubject = function(subject){
+	this.subjects.push(subject);
+	subject.save();
+};
+
+model.build = function(){
+	this.makeModels([Category, Subject, Message]);
+
+	this.collection(Category, {
+		sync: function(){
+			http().get('/forum/public/json/categories.json').done(function(categories){
+				this.load(categories);
+			}.bind(this));
+		}
+	})
+};
