e62fa323640dc10e943654bf285e9f28607a0fb3
dboissin <damien.boissin@web-education.net>
2014-07-04 10:07:11 +0200

    Fix repository

--- src/main/java/fr/wseduc/forum/Forum.java
+++ src/main/java/fr/wseduc/forum/Forum.java
@@ -1,22 +1,14 @@
 package fr.wseduc.forum;
 
-import fr.wseduc.forum.controllers.ActionFilter;
 import fr.wseduc.forum.controllers.ForumController;
-import fr.wseduc.webutils.Server;
-import fr.wseduc.webutils.request.filter.SecurityHandler;
+import org.entcore.common.http.BaseServer;
 
-public class Forum extends Server {
+public class Forum extends BaseServer {
 
 	@Override
 	public void start() {
 		super.start();
-
-		ForumController controller = new ForumController(vertx, container, rm, securedActions);
-		controller.get("", "view");
-
-		SecurityHandler.addFilter(
-				new ActionFilter(controller.securedUriBinding(), container.config(), vertx)
-		);
+		addController(new ForumController());
 	}
 
 }
