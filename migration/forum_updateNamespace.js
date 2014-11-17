0847843538cd2f5cc552d56cf9064a7e6d201808
Zakaria Kourrida <zakaria.kourrida@atos.net>
2014-11-17 17:33:50 +0100

    [Forum] #2348 namespace-atos correction

--- migration/forum_updateNamespace.js
+++ migration/forum_updateNamespace.js
@@ -1,10 +1,10 @@
-db.forum.categories.find({"shared.0" : { "$exists" : true }}, {"_id":1, "shared":1}).forEach(function(doc) {
-  var string = JSON.stringify(doc);
+db.forum.categories.find({"shared.0" : { "$exists" : true }}, {"_id":1, "shared":1}).forEach(function(cat) {
+  var string = JSON.stringify(cat);
   var obj = JSON.parse(string.replace(/fr\-wseduc/g, 'net-atos-entng'));
-  db.forum.categories.update({"_id" : doc._id}, { $set : { "shared" : obj.shared}});
+  db.forum.categories.update({"_id" : cat._id}, { $set : { "shared" : obj.shared}});
 });
-db.forum.subjects.find({"shared.0" : { "$exists" : true }}, {"_id":1, "shared":1}).forEach(function(doc) {
-  var string = JSON.stringify(doc);
+db.forum.subjects.find({"shared.0" : { "$exists" : true }}, {"_id":1, "shared":1}).forEach(function(sub) {
+  var string = JSON.stringify(sub);
   var obj = JSON.parse(string.replace(/fr\-wseduc/g, 'net-atos-entng'));
-  db.forum.subjects.update({"_id" : doc._id}, { $set : { "shared" : obj.shared}});
+  db.forum.subjects.update({"_id" : sub._id}, { $set : { "shared" : obj.shared}});
 });
